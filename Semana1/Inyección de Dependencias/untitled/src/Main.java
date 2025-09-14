2fe44293e7251d12e22e6f85839cbf588fd46586
Alan Flores <alanpfqa@gmail.com>
2025-09-13 20:58:18 -0600

    Se anexa evidencia simulador

---
+++ Semana1/Inyección de Dependencias/untitled/src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        try{
+            System.exit(0);
+        }
+        finally{
+            System.out.println("finally is always executed!");      }   } }
