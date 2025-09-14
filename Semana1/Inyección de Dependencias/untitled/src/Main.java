679929f0aa173cba5a0eb1dce7d6c93b5919f44d
Alan Flores <alanpfqa@gmail.com>
2025-09-13 21:01:42 -0600

    Se corrigen carpetas en Inyeccion dependencias

--- Semana1/Inyección de Dependencias/untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        try{
-            System.exit(0);
-        }
-        finally{
-            System.out.println("finally is always executed!");      }   } }
