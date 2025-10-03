54734f3aec13e6752a40715b6dfea54d966fc595
Alan Flores <alanpfqa@gmail.com>
2025-10-03 12:13:37 -0600

    Se agregan DTO del Modulo Experiment

--- Semana4/SistemaDeGestionLaboratorio/src/main/java/com/ArgonLaboratory/SistemaDeGestionLaboratorio/Experiment/dto/ExperimentRequest.java
+++ Semana4/SistemaDeGestionLaboratorio/src/main/java/com/ArgonLaboratory/SistemaDeGestionLaboratorio/Experiment/dto/ExperimentRequest.java
@@ -1,4 +1,25 @@
 package com.ArgonLaboratory.SistemaDeGestionLaboratorio.Experiment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExperimentRequest {
+
+    @NotBlank(message = "Es requerido el nombre del experimento")
+    private String name;
+
+    @NotNull(message = "El status del experimento es necesario")
+    private String status;
+
+    @NotNull(message = "El nivel de riesgo del experimento es necesario")
+    private String risk;
+
+    @NotNull(message = "La cedula del investigador es necesaria")
+    private String investigatorLicenseNumber;
 }
--- Semana4/SistemaDeGestionLaboratorio/src/main/java/com/ArgonLaboratory/SistemaDeGestionLaboratorio/Experiment/dto/ExperimentResponse.java
+++ Semana4/SistemaDeGestionLaboratorio/src/main/java/com/ArgonLaboratory/SistemaDeGestionLaboratorio/Experiment/dto/ExperimentResponse.java
@@ -1,4 +1,38 @@
 package com.ArgonLaboratory.SistemaDeGestionLaboratorio.Experiment.dto;
 
+import com.ArgonLaboratory.SistemaDeGestionLaboratorio.Experiment.model.Experiment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExperimentResponse {
+    private Long id;
+    private String name;
+    private String folio;
+    private String description;
+    private Experiment.ExperimentStatus status;
+    private Experiment.levelRisk risk;
+    private String investigatorLicenseNumber;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ExperimentResponse fromEntity(Experiment experiment) {
+        return new ExperimentResponse(
+                experiment.getId(),
+                experiment.getName(),
+                experiment.getFolio(),
+                experiment.getDescription(),
+                experiment.getStatus(),
+                experiment.getRisk(),
+                experiment.getInvestigator().getLicenseNumber(),
+                experiment.getCreatedAt(),
+                experiment.getUpdatedAt()
+        );
+    }
+    
 }
